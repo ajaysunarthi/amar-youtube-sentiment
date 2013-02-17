270cd33b1ea5d816ddff3fbc6f6de2acd6c30806
amar.kris@gmail.com <amar.kris@gmail.com@f40dc015-db31-2c12-6a4d-d2e39cde7620>
2013-02-17 01:03:11 +0000

    video_id - no hard coding, url passing directly to command line

---
+++ comments_YT2.1.py
@@ -0,0 +1,36 @@
+from gdata.youtube import service
+from urllib import urlopen, unquote; 
+from urlparse import parse_qs, urlparse; 
+import sys;
+import codecs;
+
+
+if len(sys.argv)!= 2:
+	sys.exit("must provide the URL")
+	
+youtube_watchurl=sys.argv[1]; 
+VIDEO_ID= parse_qs(urlparse(youtube_watchurl).query)['v'][0]; 	
+
+USERNAME = 'amar.kris@gmail.com'
+PASSWORD = 'tirupatitirupati1!!'
+"""VIDEO_ID = 'pdcwbQa7S5E'"""
+
+def comments_generator(client, video_id):
+    comment_feed = client.GetYouTubeVideoCommentFeed(video_id=video_id)
+    while comment_feed is not None:
+        for comment in comment_feed.entry:
+             yield comment
+        next_link = comment_feed.GetNextLink()
+        if next_link is None:
+             comment_feed = None
+        else:
+             comment_feed = client.GetYouTubeVideoCommentFeed(next_link.href)
+
+client = service.YouTubeService()
+client.ClientLogin(USERNAME, PASSWORD)
+
+for comment in comments_generator(client, VIDEO_ID):
+	author_name = comment.author[0].name.text
+	text = comment.content.text
+	print("{}: {}".format(author_name, text))
+	
